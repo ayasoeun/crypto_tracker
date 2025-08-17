8a191e9bbc43c99993f9f8a1824fa46def90bcd6
kissess <0dmswl2@gmail.com>
2025-08-17 16:44:06 +0900

    form--handleSubmit, required, pattern

--- src/toDoList.tsx
+++ src/toDoList.tsx
@@ -1,53 +1,87 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFormState } from "react-hook-form";
 
-/* function ToDoList() {
-    const [toDo, setToDo] = useState("");
-    const [toDoError, setToDoError] = useState("");
+// function ToDoList() {
+//     const [toDo, setToDo] = useState("");
+//     const [toDoError, setToDoError] = useState("");
 
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value }, // value is in currenValue property
-        } = event;
-        setToDo(value);
-        setToDoError(""); // put here!! to check conditions of every changes made
-    };
+//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//         const {
+//             currentTarget: { value }, // value is in currenValue property
+//         } = event;
+//         setToDo(value);
+//         setToDoError(""); // put here!! to check conditions of every changes made
+//     };
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(toDo);
-        if (toDo.length < 10) {
-            setToDoError("toDo should longer than 10");
-        }
-        console.log("submit");
-    };
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={toDo}
-                    placeholder="Write a to do"
-                    ty pe="text"
-                />
-                <button>Add</button>
-                {toDoError !== "" ? toDoError : null}
-            </form>
-        </div>
-    );
-}
- */
+//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         console.log(toDo);
+//         if (toDo.length < 10) {
+//             setToDoError("toDo should longer than 10");
+//         }
+//         // console.log("submit");
+//     };
+//     return (
+//         <div>
+//             <form onSubmit={onSubmit}>
+//                 <input
+//                     onChange={onChange}
+//                     value={toDo}
+//                     placeholder="Write a to do"
+//                     type="text"
+//                 />
+//                 <button>Add</button>
+//                 {toDoError !== "" ? toDoError : null}
+//             </form>
+//         </div>
+//     );
+// }
+
 function ToDoList() {
-    const { register, watch } = useForm();
-    // console.log(register("toDo")); //returns an object
-    console.log(watch());
+    const { register, handleSubmit, formState } = useForm();
+    const onValid = (data: any) => {
+        console.log(data);
+    };
+    console.log(formState.errors);
     return (
         <div>
-            <form>
-                <input {...register("email")} placeholder="Email" />
-                <input {...register("password")} placeholder="Password" />
-                <input {...register("number")} placeholder="Phone Number" />
-                <input {...register("address")} placeholder="Address" />
+            <form
+                style={{ display: "grid", gap: 10, maxWidth: 200 }}
+                onSubmit={handleSubmit(onValid)}
+            >
+                <input
+                    {...register("email", {
+                        required: true,
+                        minLength: 10,
+                        pattern: {
+                            value: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+                            message: "Only correct email forms allowed ",
+                        },
+                    })}
+                    placeholder="Email"
+                />
+                <input
+                    {...register("firstname", { required: true })}
+                    placeholder="First Name"
+                />
+                <input
+                    {...register("lastname", { required: true })}
+                    placeholder="Last Name"
+                />
+                <input
+                    {...register("username", { required: true })}
+                    placeholder="Username"
+                />
+                <input
+                    {...register("password", {
+                        required: true,
+                        pattern: {
+                            value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+                            message: "Only correct password allowed",
+                        },
+                    })}
+                    placeholder="Password"
+                />
                 <button>Add</button>
             </form>
         </div>
